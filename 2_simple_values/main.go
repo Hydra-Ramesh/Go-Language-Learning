d8a436d20c78dad04015546130be0f3bf22fdb39
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-11-26 14:09:33 +0530

    Learn Variable Printing

---
+++ 2_simple_values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Integer
+	fmt.Println(1 + 1)
+	// String
+	fmt.Println("Go" + "Lang")
+	// Boolean
+	fmt.Println(true)
+	// Float
+	fmt.Println(3.14)
+}
